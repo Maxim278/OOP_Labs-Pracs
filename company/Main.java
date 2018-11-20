6f46b7cc395f0771db62c0899145c3ae60a74cd5
Maxim278 <31596111+Maxim278@users.noreply.github.com>
2018-11-20 21:36:10 +0300

    Lab1

---
+++ company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=1;i<11;i++){
+            float x=1f/i;
+            System.out.println(x);
+        }
+    }
+}
